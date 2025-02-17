07d0cbe158263fef725824b9f81d189498aed5ee
jihtin <129176693+jihtin@users.noreply.github.com>
2025-02-17 23:20:57 +0530

    feate : comment

--- src/App.jsx
+++ src/App.jsx
@@ -6,43 +6,54 @@ const App = () => {
    <>
      <div className="min-h-screen bg-slate-50 dark:bg-black dark:text-white">
 
+
+           {/* navbar header section  */}
+
          <header className="bg-teal-700 text-white sticky top-0 z-10">
 
-          <section className="max-w-4xl mx-auto p-4 flex justify-between items-center">
-                <h1 className='text-3xl font-medium'> 
-                  <a href="#hero">  🚀 Acme Rokects</a> 
-                </h1>
-                  <button id='mobile-open-button' className='text-3xl sm:hidden focus:outline-none'>
-                    &#9776;
-                  </button> 
-               <nav className='hidden sm:block space-x-8 text-xl ' aria-label='main' >
-                    <a href="#rokect" className='hover:opacity-90'> Our Rocket </a>
-                    <a href="#textimonials" className='hover:opacity-90'>Textimonials</a>
-                    <a href="#condact" className='hover:opacity-90'>Contact Us</a>
-               </nav>
+            <section className="max-w-4xl mx-auto p-4 flex justify-between items-center">
+                  <h1 className='text-3xl font-medium'> 
+                    <a href="#hero">  🚀 Acme Rokects</a> 
+                  </h1>
+                  {/* hamburger button */}
 
-          </section>
+                    <button id='mobile-open-button' className='text-3xl sm:hidden focus:outline-none'> &#9776; </button> 
+                            
+                           {/* navbar items  */}
+                          <nav className='hidden sm:block space-x-8 text-xl ' aria-label='main' >
+                                <a href="#rokect" className='hover:opacity-90'> Our Rocket </a>
+                                <a href="#textimonials" className='hover:opacity-90'>Textimonials</a>
+                                <a href="#condact" className='hover:opacity-90'>Contact Us</a>
+                          </nav>
+            </section>
          </header>
 
 
+                {/* main section  */}
            <main className='max-w-4xl mx-auto'>
 
             <section id='hero' className='flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12'> 
               
               <article className='sm:w-1/2' >
-              <h2 className='max-w-md mx-auto text-4xl font-bold text-cnter sm:text-5xl sm:text-left text-salte-900 dark:text-white'>
+               {/* content  */}
 
+              <h2 className='max-w-md mx-auto text-4xl font-bold text-cnter sm:text-5xl sm:text-left text-salte-900 dark:text-white'>
                 We Boldy  <span className='text-indigo-700 dark:text-indigo-300'> Go Where No Rocket Has</span> Gone Before...
               </h2>
-               
+
+                {/* paragraph content section  */}
+
                <p className='max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400 '> We're building rockets for the next century today.  from the moon to Mars , Jubiter and byond.</p>
                <p className='max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400 '>Think Aceme Rokets . </p>
 
               </article>
+                {/* img rocket  */}
 
               <img className='w-1/2' src={rocketdab} alt="rocketdab" />
                </section>
-              
+
+                {/* underline  */}
+                
                <hr className='mx-auto dark:bg-white w-1/2' />
 
                <section id='rockets' className='p-6 my-12 ' >
